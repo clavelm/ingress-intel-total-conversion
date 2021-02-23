fe82e9897ff20f6d82ee201c0249559596e870b8
Mathieu CLAVEL <math.clavel@protonmail.com>
2021-02-23 22:12:50 +0100

    plugin/ssp: Configure shareLink in setup()
    jQ was not available when used in the global section.

--- plugins/share-selected-portal.js
+++ plugins/share-selected-portal.js
@@ -7,15 +7,7 @@
 // use own namespace for plugin
 window.plugin.ssp = function() {};
 
-window.plugin.ssp.shareLink = $('<a>')
-  .addClass('shareLink')
-  .text('⇛')
-  .css('display', 'inline-block')
-  .css('float', 'left')
-  .css('margin', '3px 1px 0 4px')
-  .css('width', '16px')
-  .css('height', '15px')
-  .css('overflow', 'hidden');
+window.plugin.ssp.shareLink = undefined;
 
 // Append a share link in sidebar.
 window.plugin.ssp.onPortalDetailsUpdated = function() {
@@ -48,6 +40,25 @@ var setup = function() {
   if (typeof android !== 'undefined' && android && android.intentPosLink) {
     window.addHook('portalDetailsUpdated', window.plugin.ssp.onPortalDetailsUpdated);
     window.addHook('portalSelected', window.plugin.ssp.onPortalSelected);
+
+    var span = $('<span>')
+      .css('background-image', 'url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAQAAAC1+jfqAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QAAKqNIzIAAAAJcEhZcwAADsQAAA7EAZUrDhsAAAAHdElNRQflAhcVAAgpZfV+AAAAw0lEQVQoz3WRsQ7BYBSFj8YgdGLxDiab6EMw4QmsXqCtF7F3kngMsRiwsti1UdFK5DP4G8HfM93knuR+51zJIubExISyiwlXAFLGkmNxDORKkhoaWgx01TNjpsPvss2CJxuOJCTscIubEQEtZiScmeJQpU+fakGcAhkPUkLq/8wxb+UMbZkc8fHaU4dcgYy85ITEiAif5hekh2cgS2LGJOxNzN+iOBnsO76lycpWazPW1LH/YqWbJCnVsuyfARcu+JL0AjA1mAmE9DgjAAAAJXRFWHRkYXRlOmNyZWF0ZQAyMDIxLTAyLTIzVDIxOjAwOjA4KzAwOjAwneNzuQAAACV0RVh0ZGF0ZTptb2RpZnkAMjAyMS0wMi0yM1QyMTowMDowOCswMDowMOy+ywUAAAAZdEVYdFNvZnR3YXJlAHd3dy5pbmtzY2FwZS5vcmeb7jwaAAAAAElFTkSuQmCC")')
+      .css('background-size', 'cover')
+      .css('background-repeat', 'no-repeat')
+      .css('display', 'inline-block')
+      .css('float', 'left')
+      .css('margin', '3px 1px 0 4px')
+      .css('width', '16px')
+      .css('height', '15px')
+      .css('overflow', 'hidden');
+
+    window.plugin.ssp.shareLink = $('<a>')
+      .addClass('shareLink')
+      .css('float', 'left')
+      .css('margin', '-19px 0 0 -5px')
+      .css('padding', '0 3px 1px 4px')
+      .css('background', '#262c32')
+      .append(span);
   }
 
 };
